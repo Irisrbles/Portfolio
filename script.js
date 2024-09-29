a403655771a763020f5181b20c8ab70046c49ef3
Irisrbles <148631739+Irisrbles@users.noreply.github.com>
2024-09-29 00:55:54 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// Toggle Navbar
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('#navbar');
+
+menuIcon.onclick = () => {
+    navbar.classList.toggle('active');
+};
+
+// Typewriter Effect
+const typewriterText = document.querySelector('.typewriter-text');
+const textArray = typewriterText.innerHTML.split('');
+typewriterText.innerHTML = '';
+
+let i = 0;
+const typeWriter = () => {
+    if (i < textArray.length) {
+        typewriterText.innerHTML += textArray[i];
+        i++;
+        setTimeout(typeWriter, 100);
+    }
+};
+
+window.onload = typeWriter;
